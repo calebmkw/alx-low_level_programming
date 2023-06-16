76b2d0ba5ee40104cdd78293b25345512b25b5bb
calebmkw <calebmkw@gmail.com>
2023-06-16 14:15:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
-#!/bin/bash
-/**
- * main - Entry point 
- * return : Always 0 ( succsess)
- */
 #include <stdio.h>
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
-} 
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
+ 
+
 
 
