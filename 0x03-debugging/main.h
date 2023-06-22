74767fcba67135cd14758432a75e833a5b449292
calebmkw <calebmkw@gmail.com>
2023-06-22 13:39:22 -0700

    it was rally hard

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,29 @@
-ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <stdio.h>
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+int largest_number(int a, int b, int c)
+{
+    int largest;
 
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
 
-#endif
+    return (largest);
+}
