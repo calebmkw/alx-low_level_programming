ddb88b0200792a7e2b8fe75334a67d509b40271e
calebmkw <calebmkw@gmail.com>
2023-06-26 13:19:48 -0700

    betty you are hard

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
-*
-* reset_to_98 - reset argument to 98
-* @n: printed integer
-* Return: void
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By Bwave/Bright
+*  Return: 0 is success
 */
 
 void reset_to_98(int *n)
 {
-
-   *n = 98;
-
+	*n = 98;
 }
