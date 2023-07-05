f5632de87c138d290151f2d636058f353e520d11
calebmkw <calebmkw@gmail.com>
2023-07-05 07:05:14 -0700

    chalanging

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
-#include"main.h"
+#include<stdio.h>
 
 /**
  * print_alphabet - utilizes on the _putchar function to print
  *                 the alphabet a - z
 */
 
-void print_alphabet(void)
+int main()
 {
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ++ch)
-		_putchar(ch);
-	_putchar('\n');
+		putchar(ch);
+	putchar('\n');
+	return 0;
 }
